91536c7666d6d5c96d63224378d910798dd9c009
Anson Lee <a.lee3213@hotmail.com>
2021-08-08 22:29:20 +1000

    Add small db insert query to test mongoDB

--- backend/controllers/pong.go
+++ backend/controllers/pong.go
@@ -1,11 +1,17 @@
 package controllers
 
 import (
-    "github.com/gin-gonic/gin"
+    "context"
     "net/http"
+
+    "github.com/anslee/nomad/db"
+    "github.com/anslee/nomad/models/user"
+    "github.com/gin-gonic/gin"
 )
 
 func Pong(c *gin.Context) {
+    newUser := user.User{Name: "Anson", Age: 21, City: "Home"}
+    db.GetCollection(user.COLLECTION_NAME).InsertOne(context.Background(), newUser)
     c.JSON(http.StatusOK, gin.H{
         "message": "pong!",
     })
---
+++ backend/models/user/user.go
@@ -0,0 +1,9 @@
+package user
+
+const COLLECTION_NAME = "user"
+
+type User struct {
+    Name string
+    Age int
+    City string
+}
