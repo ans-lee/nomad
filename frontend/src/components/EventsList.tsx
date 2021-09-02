c7761cbf61abd0f5e28f307732dd7e3c4e0fea66
Anson Lee <a.lee3213@hotmail.com>
2021-09-02 18:00:48 +1000

    Add mapping function for the events

--- frontend/src/components/EventsList.tsx
+++ frontend/src/components/EventsList.tsx
@@ -1,22 +1,50 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 
-const EventsList: React.FC = () => {
-  return (
-    <div className="flex flex-col">
-      <div className="flex w-full mt-10">
-        <div className="rounded-md border border-gray-300 w-72 h-48"></div>
-      </div>
-      <div className="flex w-full mt-10">
-        <div className="rounded-md border border-gray-300 w-72 h-48"></div>
-      </div>
-      <div className="flex w-full mt-10">
-        <div className="rounded-md border border-gray-300 w-72 h-48"></div>
-      </div>
-      <div className="flex w-full mt-10">
-        <div className="rounded-md border border-gray-300 w-72 h-48"></div>
+const events = [
+  {
+    title: 'Vivid 2022',
+    location: 'Sydney Harbour Bridge',
+    start: '5/9/2021',
+    end: '6/9/2021',
+    description: `Sydney's biggest annual lights show!`,
+    category: 'Festival',
+    online: false,
+  },
+  {
+    title: 'Vivid 2022',
+    location: 'Sydney Harbour Bridge',
+    start: '5/9/2021',
+    end: '6/9/2021',
+    description: `Sydney's biggest annual lights show!`,
+    category: 'Festival',
+    online: false,
+  },
+  {
+    title: 'Vivid 2022',
+    location: 'Sydney Harbour Bridge',
+    start: '5/9/2021',
+    end: '6/9/2021',
+    description: `Sydney's biggest annual lights show!`,
+    category: 'Festival',
+    online: false,
+  },
+];
+
+const getEvents = events.map((item, key) => (
+  <Fragment key={key}>
+    <hr className="my-2" key={`border ${key}`} />
+    <div className="flex w-full my-10">
+      <div className="rounded-md border border-gray-300 w-72 h-48 mr-4"></div>
+      <div className="flex flex-col w-1/2">
+        <div className="text-xl mb-2">{item.title}</div>
+        <div className="text-sm mb-2">{`${item.start} - ${item.end}`}</div>
+        <div className="text-sm mb-4">{item.location}</div>
+        <div className="break words">{item.description}</div>
       </div>
     </div>
-  );
-};
+  </Fragment>
+));
+
+const EventsList: React.FC = () => <div className="flex flex-col">{getEvents}</div>;
 
 export default EventsList;
