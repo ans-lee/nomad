ea3f887654af221d83ff2ddde3cc8a5c62b3a8d5
Anson Lee <a.lee3213@hotmail.com>
2021-09-09 21:54:33 +1000

    Improve gin gonic backend script

--- backend/run_server.sh
+++ backend/run_server.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-export GIN_MODE=release
+if [ "$#" -gt 1 ]; then
+    echo "Usage: bash run_server.sh <mode> (where mode is production or dev or none for dev)"
+    exit
+elif [[ $1 == "production" ]]; then
+    export GIN_MODE=release
+fi
+
 go run server.go
